45c88dc149ebde84acd51b9a28ac354f9a2deda8
arader37 <50584667+arader37@users.noreply.github.com>
2019-09-13 16:38:44 -0500

    Update scanner_releases_gantt.php

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -7,24 +7,70 @@
   include("./nav.php");
   global $db;
 
-  ?>
+?>
+<html>
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load('current', {'packages':['gantt']});
+    google.charts.setOnLoadCallback(drawChart);
 
+    function drawChart() {
 
-<div class="right-content">
-    <div class="container">
+      var data = new google.visualization.DataTable();
+      
+      data.addColumn('string', 'Task ID');
+      data.addColumn('string', 'Task Name');
+      data.addColumn('string', 'Type');
+      data.addColumn('date', 'Start Date');
+      data.addColumn('date', 'End Date');
+      data.addColumn('number', 'Duration');
+      data.addColumn('number', 'Percent Complete');
+      data.addColumn('string', 'Dependencies');
 
-      <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
+      data.addRows([
+        <?php 
+          $query = "SELECT * from releases";
+          $exec = mysqli_query($db,$query);
+          while($row = mysqli_fetch_array($exec)){
+            if ($row['status']=="Draft"){
+              $status = 25;
+            }elseif($row['status']=="Active"){
+              $status = 50;
+            }elseif($row['status']=="Completed"){
+              $status = 75;
+            }else{
+              $status = 100;
+            };
+            $startdt = explode('-',$row['open_date']);
+            $enddt = explode('-',$row['rtm_date']);
+            echo "['".$row['id']."','"
+                  .$row['name']."','"
+                  .$row['type']."',
+                  new Date(".$startdt[0].", ".$startdt[1].", ".$startdt[2]."),
+                  new Date(".$enddt[0].", ".$enddt[1].", ".$enddt[2]."),null,"
+                  .$status.",null],";
+          };
+        ?>
+      ]);
 
-    
-        
-        TODO: Gantt Chart Code
+      var options = {
+        height: 1000,
+        width: 5000,
+        gantt: {
+          trackHeight: 30
+        }
+      };
 
-        
+      var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
 
- <style>
-   tfoot {
-     display: table-header-group;
-   }
- </style>
+      chart.draw(data, options);
+    }
+  </script>
+</head>
+<body>
+  <div id="chart_div"></div>
+</body>
+</html>
 
   <?php include("./footer.php"); ?>
